b879d45eefe03ffe2c5658a87662bb3aa3fbf68a
Goggwell <zulkarnaensamuel1@gmail.com>
2023-11-04 22:12:58 +0700

    chore: vercel deployment

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,9 @@
 import { Kysely } from "kysely";
 import { NeonDialect } from "kysely-neon";
 import { DB } from "./types";
-import ws from "ws";
 
 export const db = new Kysely<DB>({
   dialect: new NeonDialect({
     connectionString: process.env.DATABASE_URL,
-    webSocketConstructor: ws,
   }),
 });
