18decab78a59b3b8d56c906f1beac4c9cbbae1a5
samuel-bd <samuely@frisseblikken.com>
2023-11-04 19:43:36 +0700

    chore: adding options to trpc context

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,17 @@
 import { initTRPC } from "@trpc/server";
 import { z } from "zod";
+import SuperJSON from "superjson";
 
 import { Context } from "./context";
 
-export const t = initTRPC.context<Context>().create();
+export const t = initTRPC.context<Context>().create({
+  transformer: SuperJSON,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
 
+// keeping all routes here for simplicity
 export const appRouter = t.router({
   getMonsters: t.procedure.query(async ({ ctx }) => {
     return await ctx.db.selectFrom("beasts").selectAll().execute();
