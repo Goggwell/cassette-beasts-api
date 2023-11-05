a5ffcdb169df1a27bce8fe9ebb9ae90fcc51eb66
Goggwell <zulkarnaensamuel1@gmail.com>
2023-11-06 04:18:12 +0700

    feat(router): new route for future pagination/infinite scrolling

--- src/router.ts
+++ src/router.ts
@@ -14,6 +14,21 @@ export const appRouter = t.router({
   getMonsters: t.procedure.query(async ({ ctx }) => {
     return await ctx.db.selectFrom("beasts").selectAll().execute();
   }),
+  getMonstersOffset: t.procedure // e.g. http://localhost:${env.PORT}/api/getMonstersOffset?input={"limit":2,"offset":2}
+    .input(
+      z.object({
+        limit: z.number(),
+        offset: z.number(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      return await ctx.db
+        .selectFrom("beasts")
+        .selectAll()
+        .limit(input.limit)
+        .offset(input.offset)
+        .execute();
+    }),
   getMonsterById: t.procedure
     .input(z.number())
     .query(async ({ input, ctx }) => {
